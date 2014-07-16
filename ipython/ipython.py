56ac903979990f9e23cc52014628c61892c22c5a
Neurolit <neurolit@gmail.com>
2014-07-16 20:05:06 +0200

    Remove CSS loading in studio_view()

--- ipython/ipython.py
+++ ipython/ipython.py
@@ -92,7 +92,6 @@ class IPythonNotebookXBlock(XBlock):
 
         frag = Fragment()
         frag.add_content(render_template('/templates/html/ipython-edit.html', context))
-        frag.add_css(self.resource_string("static/css/ipython.css"))
         frag.add_javascript(self.resource_string("static/js/src/ipython-edit.js"))
         frag.initialize_js('IPythonNotebookXBlock')
         return frag
