3743522f0164b20e7423edb81467a7c01d20742c
Neurolit <neurolit@gmail.com>
2014-10-15 01:39:56 +0200

    Init imageResizer outside of function ($)
    function ($) is sometimes not triggered in a XBlock

--- ipython/static/js/src/ipython.js
+++ ipython/static/js/src/ipython.js
@@ -1,10 +1,9 @@
 /* Javascript for IPythonNotebookXBlock. */
 function IPythonNotebookXBlock(runtime, element) {
-
+    $('iframe.notebook').iFrameResize({ log: false,
+                                        heightCalculationMethod: "lowestElement",
+                                        checkOrigin: false });
     $(function ($) {
         /* Here's where you'd do things on page load. */
-        $('iframe.notebook').iFrameResize({ log: false,
-                                            heightCalculationMethod: "lowestElement",
-                                            checkOrigin: false});
     });
 }
